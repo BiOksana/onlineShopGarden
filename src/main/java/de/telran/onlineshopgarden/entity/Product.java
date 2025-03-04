d12ac67d71442724707e2c999b24f0572930f398
Yevhen Kariev <yevhenkariev@gmail.com>
2025-03-04 13:43:50 +0100

    deleted Unused import statement

--- src/main/java/de/telran/onlineshopgarden/entity/Product.java
+++ src/main/java/de/telran/onlineshopgarden/entity/Product.java
@@ -1,6 +1,5 @@
 package de.telran.onlineshopgarden.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
