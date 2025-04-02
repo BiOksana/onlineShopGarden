bccc3c827984790471d944b34b1a6c9322bb1a71
rajatabla <vgubinbox@gmail.com>
2025-04-02 18:11:05 +0200

    SCRUM 58: Project documentation added + README.md

--- src/main/java/de/telran/onlineshopgarden/controller/CartController.java
+++ src/main/java/de/telran/onlineshopgarden/controller/CartController.java
@@ -3,6 +3,8 @@ package de.telran.onlineshopgarden.controller;
 import de.telran.onlineshopgarden.dto.CartDto;
 import de.telran.onlineshopgarden.dto.CartItemAddDto;
 import de.telran.onlineshopgarden.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cart")
+@Tag(name = "Cart", description = "REST API for managing cart in the app")
 public class CartController {
     private final CartService service;
 
@@ -19,12 +22,14 @@ public class CartController {
         this.service = service;
     }
 
+    @Operation(summary = "Get cart by user id")
     @GetMapping
     public ResponseEntity<CartDto> getByUserId(@RequestParam Integer userId) {
         // TODO: JWT аутентификация
         return new ResponseEntity<>(service.getByUserId(userId), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add item to cart")
     @PostMapping
     public ResponseEntity<Void> addItem(@Valid @RequestBody CartItemAddDto dto, @RequestParam int userId) {
         // todo: replace with real userId
@@ -32,6 +37,7 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "Remove item from cart")
     @DeleteMapping
     public ResponseEntity<Void> deleteByUserId(@RequestParam Integer userId) {
         //todo: replace with real userId
--- src/main/java/de/telran/onlineshopgarden/controller/CategoryController.java
+++ src/main/java/de/telran/onlineshopgarden/controller/CategoryController.java
@@ -2,6 +2,8 @@ package de.telran.onlineshopgarden.controller;
 
 import de.telran.onlineshopgarden.dto.CategoryDto;
 import de.telran.onlineshopgarden.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
+@Tag(name = "Category", description = "REST API for managing category in the app")
 public class CategoryController {
     private final CategoryService service;
 
@@ -19,21 +22,23 @@ public class CategoryController {
         this.service = service;
     }
 
+    @Operation(summary = "Get all categories")
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getAll() {
         return ResponseEntity.ok(service.getAll());
     }
 
+    @Operation(summary = "Get category by id")
     @GetMapping("{categoryId}")
     public ResponseEntity<CategoryDto> getById(@PathVariable Integer categoryId) {
         return ResponseEntity.ok(service.getById(categoryId));
     }
-
+    @Operation(summary = "Create new category")
     @PostMapping
     public ResponseEntity<CategoryDto> create(@Valid @RequestBody CategoryDto dto) {
         return new ResponseEntity<>(service.create(dto), HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Update category by id")
     @PutMapping("{categoryId}")
     public ResponseEntity<CategoryDto> update(@PathVariable Integer categoryId, @Valid @RequestBody CategoryDto dto) {
         return ResponseEntity.ok(service.update(categoryId, dto));
